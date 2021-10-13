45205a85492b9c66d365c48dd654a53da6e4a035
dima_legan <dimasky7@yandex.ru>
2021-10-13 20:53:45 +0700

    ver. 2

---
+++ js/form.js
@@ -0,0 +1,5 @@
+/*const numberOfRooms = document.querySelector('#room_number');
+numberOfRooms.addEventListener('change', function() {
+    alert(1);
+})
+*/
--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 import './similarOffer.js';
+
+const numberOfRooms = document.querySelector('#room_number');
+numberOfRooms.addEventListener('change', function() {
+    if (numberOfRooms.value === '2') {
+        alert(1);
+    }
+});

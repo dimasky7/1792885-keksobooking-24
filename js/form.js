4ba5b5147fa64f2cb71311d298d20ffc3352b453
dima_legan <dimasky7@yandex.ru>
2021-10-14 13:15:24 +0700

    ver. 3

--- js/form.js
+++ js/form.js
@@ -1,5 +1,40 @@
-/*const numberOfRooms = document.querySelector('#room_number');
-numberOfRooms.addEventListener('change', function() {
-    alert(1);
-})
-*/
+const numberOfRooms = document.querySelector('#room_number');
+const capacity = document.querySelector('#capacity');
+const oneGuest = document.querySelector('#one_guest');
+const twoGuests = document.querySelector('#two_guests');
+const threeGuests = document.querySelector('#three_guests');
+const notForGuests = document.querySelector('#not_for_guests');
+
+//initial start
+twoGuests.setAttribute('disabled', 'disabled');
+threeGuests.setAttribute('disabled', 'disabled');
+notForGuests.setAttribute('disabled', 'disabled');
+
+//handlers
+numberOfRooms.addEventListener('change', () => {
+  if (numberOfRooms.value === '1') {
+    oneGuest.removeAttribute('disabled');
+    capacity.value = '1';
+    twoGuests.setAttribute('disabled', 'disabled');
+    threeGuests.setAttribute('disabled', 'disabled');
+    notForGuests.setAttribute('disabled', 'disabled');
+  } else if (numberOfRooms.value === '2') {
+    oneGuest.removeAttribute('disabled');
+    twoGuests.removeAttribute('disabled');
+    capacity.value = '2';
+    threeGuests.setAttribute('disabled', 'disabled');
+    notForGuests.setAttribute('disabled', 'disabled');
+  } else if (numberOfRooms.value === '3') {
+    oneGuest.removeAttribute('disabled');
+    twoGuests.removeAttribute('disabled');
+    threeGuests.removeAttribute('disabled');
+    capacity.value = '3';
+    notForGuests.setAttribute('disabled', 'disabled');
+  } else if (numberOfRooms.value === '100') {
+    notForGuests.removeAttribute('disabled');
+    capacity.value = '0';
+    oneGuest.setAttribute('disabled', 'disabled');
+    twoGuests.setAttribute('disabled', 'disabled');
+    threeGuests.setAttribute('disabled', 'disabled');
+  }
+});
--- js/main.js
+++ js/main.js
@@ -1,8 +1,2 @@
 import './similarOffer.js';
-
-const numberOfRooms = document.querySelector('#room_number');
-numberOfRooms.addEventListener('change', function() {
-    if (numberOfRooms.value === '2') {
-        alert(1);
-    }
-});
+import './form.js';

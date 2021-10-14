92e34cc88e2278a409366550556bc645d62e645c
dima_legan <dimasky7@yandex.ru>
2021-10-14 20:14:22 +0700

    ver. 5. Denis utomil.

--- js/form.js
+++ js/form.js
@@ -1,30 +1,30 @@
 const numberOfRooms = document.querySelector('#room_number');
 const capacity = document.querySelector('#capacity');
-const guestsNumber = document.querySelectorAll('.guestsNumber');
 
 //initial start
-for (let i = 1; i < guestsNumber.length; i++) {
-  guestsNumber[i].setAttribute('disabled', 'disabled');
+for (let i = 1; i < capacity.length; i++) {
+  capacity.children[i].setAttribute('disabled', 'disabled');
 }
 
 //handlers
 numberOfRooms.addEventListener('change', () => {
   for (let i=1; i<=3; i++) {
     if (numberOfRooms.value === `${i}`) {
-      for (let j=0; j < i; j++) {
-        guestsNumber[j].removeAttribute('disabled');
+      for (let j=0; j <=3 ; j++) {
+        if (j <= i-1) {
+          capacity.children[j].removeAttribute('disabled');
+        } else {
+          capacity.children[j].setAttribute('disabled', 'disabled');
+        }
       }
       capacity.value = `${i}`;
-      for (let j=i; j < guestsNumber.length; j++) {
-        guestsNumber[j].setAttribute('disabled', 'disabled');
-      }
     }
   }
   if (numberOfRooms.value === '100') {
-    guestsNumber[3].removeAttribute('disabled');
+    capacity.children[3].removeAttribute('disabled');
     capacity.value = '0';
-    for (let i=0; i < guestsNumber.length - 1; i++) {
-      guestsNumber[i].setAttribute('disabled', 'disabled');
+    for (let i=0; i < capacity.length - 1; i++) {
+      capacity.children[i].setAttribute('disabled', 'disabled');
     }
   }
 });

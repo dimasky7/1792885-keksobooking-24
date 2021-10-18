428ce620ef469f2d67c3f521290da0c6c54a1037
dima_legan <dimasky7@yandex.ru>
2021-10-18 20:53:19 +0700

    new version

--- js/form.js
+++ js/form.js
@@ -1,30 +1,35 @@
 const numberOfRooms = document.querySelector('#room_number');
 const capacity = document.querySelector('#capacity');
+const capacityOptions = document.querySelectorAll('#capacity option');
 
 //initial start
 for (let i = 1; i < capacity.length; i++) {
-  capacity.children[i].setAttribute('disabled', 'disabled');
+  capacityOptions[i].setAttribute('disabled', 'disabled');
 }
 
 //handlers
-numberOfRooms.addEventListener('change', () => {
-  for (let i=1; i<=3; i++) {
-    if (numberOfRooms.value === `${i}`) {
-      for (let j=0; j <=3 ; j++) {
-        if (j <= i-1) {
-          capacity.children[j].removeAttribute('disabled');
-        } else {
-          capacity.children[j].setAttribute('disabled', 'disabled');
-        }
-      }
-      capacity.value = `${i}`;
+numberOfRooms.addEventListener('change', (evt) => {
+//console.log(evt.target.value);
+for (let i = 1; i < numberOfRooms.length; i++) {
+  if (i <= evt.target.value) {
+    capacityOptions[i-1].removeAttribute('disabled');
+  } else {
+      capacityOptions[i-1].setAttribute('disabled', 'disabled');
     }
+    
+}
+
+if (evt.target.value === '100') {
+  capacityOptions[numberOfRooms.length -1].removeAttribute('disabled');
+  for (let i=0; i < numberOfRooms.length -1; i++) {
+    capacityOptions[i].setAttribute('disabled', 'disabled');
   }
-  if (numberOfRooms.value === '100') {
-    capacity.children[3].removeAttribute('disabled');
-    capacity.value = '0';
-    for (let i=0; i < capacity.length - 1; i++) {
-      capacity.children[i].setAttribute('disabled', 'disabled');
-    }
-  }
+}
+
+if (evt.target.value !== '100') {
+  capacity.value = evt.target.value;
+} else {
+  capacity.value = '0';
+}
+
 });

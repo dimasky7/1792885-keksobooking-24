1a497734f65149c711042838daf4399e7ad60e77
dima_legan <dimasky7@yandex.ru>
2021-10-19 15:07:38 +0700

    Ну come on! У меня впереди ещё куча заданий. И куча дел на работе.

--- js/form.js
+++ js/form.js
@@ -1,6 +1,6 @@
 const numberOfRooms = document.querySelector('#room_number');
 const capacity = document.querySelector('#capacity');
-const capacityOptions = document.querySelectorAll('#capacity option');
+const capacityOptions = capacity.querySelectorAll('option');
 
 //initial start
 for (let i = 1; i < capacity.length; i++) {
@@ -9,27 +9,24 @@ for (let i = 1; i < capacity.length; i++) {
 
 //handlers
 numberOfRooms.addEventListener('change', (evt) => {
-//console.log(evt.target.value);
-for (let i = 1; i < numberOfRooms.length; i++) {
-  if (i <= evt.target.value) {
-    capacityOptions[i-1].removeAttribute('disabled');
-  } else {
+  for (let i = 1; i <= numberOfRooms.length; i++) {
+    if (i <= evt.target.value) {
+      capacityOptions[i-1].removeAttribute('disabled');
+    } else {
       capacityOptions[i-1].setAttribute('disabled', 'disabled');
     }
-    
-}
 
-if (evt.target.value === '100') {
-  capacityOptions[numberOfRooms.length -1].removeAttribute('disabled');
-  for (let i=0; i < numberOfRooms.length -1; i++) {
-    capacityOptions[i].setAttribute('disabled', 'disabled');
   }
-}
 
-if (evt.target.value !== '100') {
-  capacity.value = evt.target.value;
-} else {
-  capacity.value = '0';
-}
+  if (evt.target.value === '100') {
+    capacityOptions[numberOfRooms.length -1].removeAttribute('disabled');
+    for (let i=0; i < numberOfRooms.length -1; i++) {
+      capacityOptions[i].setAttribute('disabled', 'disabled');
+    }
+    capacity.value = '0';
+  }
 
+  if (evt.target.value !== '100') {
+    capacity.value = evt.target.value;
+  }
 });

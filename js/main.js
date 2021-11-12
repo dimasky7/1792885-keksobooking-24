e79f697991cf708f015aa66805f7edb228ba2017
dima_legan <dimasky7@yandex.ru>
2021-11-13 01:39:23 +0700

    old version

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@ const mainPinMarker = L.marker(
 
 mainPinMarker.addTo(map);
 
-mainPinMarker.on('moveend', (evt) => {
+mainPinMarker.on('move', (evt) => {
   const coordinates = evt.target.getLatLng();
   const precision = 5;
   address.value = `${coordinates.lat.toFixed(precision)}, ${coordinates.lng.toFixed(precision)}`;
@@ -74,45 +74,26 @@ const createMarker = (announcement) => {
     .bindPopup(drawCard(announcement));
 };
 ////////////////////////Получение данных с сервера///////////////////////////
+const LOW_PRICE = 10000;
+const HIGH_PRICE = 50000;
 const housingType = mapFiltersForm.querySelector('#housing-type');
 const housingPrice = mapFiltersForm.querySelector('#housing-price');
 const housingRooms = mapFiltersForm.querySelector('#housing-rooms');
 const housingGuests = mapFiltersForm.querySelector('#housing-guests');
-const filterWifi = mapFiltersForm.querySelector('#filter-wifi');
-const filterDishwasher = mapFiltersForm.querySelector('#filter-dishwasher');
-const filterParking = mapFiltersForm.querySelector('#filter-parking');
-const filterWasher = mapFiltersForm.querySelector('#filter-washer');
-const filterElevator = mapFiltersForm.querySelector('#filter-elevator');
-const filterConditioner = mapFiltersForm.querySelector('#filter-conditioner');
-
+const housingFeatures = mapFiltersForm.querySelector('#housing-features');
 
 const getFeaturesFiltersArray = function () {
-  const array = [];
-  if (filterWifi.checked) {array.push('wifi');}
-  if (filterDishwasher.checked) {array.push('dishwasher');}
-  if (filterParking.checked) {array.push('parking');}
-  if (filterWasher.checked) {array.push('washer');}
-  if (filterElevator.checked) {array.push('elevator');}
-  if (filterConditioner.checked) {array.push('conditioner');}
-  return array;
+const featuresArray = housingFeatures.querySelectorAll('.map__checkbox:checked');
+const featuresArrayOut = Array.from(featuresArray, input => input.value);
+return featuresArrayOut;
 };
 
 const featuresMatch = function (features, filtersArray) {
-  if (features) {
-    let result;
-    for (let i = 0; i < filtersArray.length; i++) {
-      for (let j = 0; j < features.length; j++) {
-        if (! features[j].includes(filtersArray[i])) {
-          result = false;
-        } else {
-          result = true;
-          break;
-        }
-      }
-    }
-    return result;
+  if ((!features) && (filtersArray.length > 0)) {return false;}
+  for (let i=0; i < filtersArray.length; i++) {
+    if (features.includes(filtersArray[i]) === false) {return false;}
   }
-  return false;
+  return true;
 };
 
 fetch('https://24.javascript.pages.academy/keksobooking/data')
@@ -131,21 +112,33 @@ fetch('https://24.javascript.pages.academy/keksobooking/data')
       const filtersArray1 = getFeaturesFiltersArray();
       const filteredData = data.filter((announcement) => {
         let price = '';
-        if (announcement.offer.price < 10000) {
+        if (announcement.offer.price < LOW_PRICE) {
           price = 'low';
         }
-        else if (announcement.offer.price >= 10000 && announcement.offer.price <= 50000) {
+        else if (announcement.offer.price >= LOW_PRICE && announcement.offer.price <= HIGH_PRICE) {
           price = 'middle';
         }
-        else if (announcement.offer.price > 50000) {
+        else if (announcement.offer.price > HIGH_PRICE) {
           price = 'high';
         }
-        const result = (announcement.offer.type === housingType.value || housingType.value === 'any') &&
-        (`${announcement.offer.rooms}` === housingRooms.value || housingRooms.value === 'any') &&
-        (`${announcement.offer.guests}`=== housingGuests.value || housingGuests.value === 'any') &&
-        (price === housingPrice.value || housingPrice.value === 'any') && (featuresMatch(announcement.offer.features, filtersArray1));
+        const typeMatch = function () {
+          return (announcement.offer.type === housingType.value || housingType.value === 'any');
+        };
+        const roomsMatch = function () {
+          return (`${announcement.offer.rooms}` === housingRooms.value || housingRooms.value === 'any');
+        };
+        const guestsMatch = function () {
+          return (`${announcement.offer.guests}`=== housingGuests.value || housingGuests.value === 'any');
+        };
+        const priceMatch = function () {
+          return (price === housingPrice.value || housingPrice.value === 'any');
+        };
+        const result = typeMatch() && roomsMatch() && guestsMatch() && priceMatch() &&
+        featuresMatch(announcement.offer.features, filtersArray1);
+
         return result;
       });
+      console.log(filteredData);
       markerGroup.clearLayers();
       filteredData.forEach((announcement) => {
         createMarker(announcement);
